6ea2d229499c4c31ac9ad148295c3f116c9698c6
Greg Allen <me@jga.me>
2016-02-26 13:33:01 -0800

    log errors

--- index.js
+++ index.js
@@ -7,6 +7,16 @@ exports.register = function(server, options, next) {
   server.on('log', (event, tags) => {
     log(event.tags, event.data);
   });
+
+  server.on('request-internal', (request, event, tags) => {
+    if (tags.error) {
+      const data = {
+        message: event.data.message,
+        stack: event.data.stack
+      };
+      log(event.tags, data);
+    }
+  });
   next();
 };
 
