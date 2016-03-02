047ec756ddf03056771d11e1fd8381633916d49b
Greg Allen <me@jga.me>
2016-03-01 17:00:28 -0800

    added test route to show errors

--- example/server.js
+++ example/server.js
@@ -8,6 +8,11 @@ server.connection({ port: 8085 });
 server.register({
   register: require('../'),
   options: {
+    renderOptions: {
+      console: {
+        pretty: true
+      }
+    }
 
   }
 }, (err) => {
@@ -18,6 +23,14 @@ server.register({
     if (startErr) {
       throw startErr;
     }
+
+    server.route({
+      path: '/error',
+      method: 'get',
+      handler: function(request, reply) {
+        reply(new Error('some error here'));
+      }
+    });
 
     server.log(['start'], { message: 'server started', uri: server.info.uri });
   });
