e0019a9feb55878f6a0c7892a6a914d71e7f886f
Chris <orthagonal1@gmail.com>
2018-01-31 16:50:48 -0600

    just pass server.logs to logr

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 'use strict';
 
 const Logr = require('logr');
-const userAgentLib = require('useragent');
 
-const register = async function(server, options) {
+const register = function(server, options) {
   const log = Logr.createLogger(options);
 
   server.events.on('log', (event, tags) => {
@@ -12,35 +11,6 @@ const register = async function(server, options) {
     }
     log(event.tags, event.data, {});
   });
-  if (options.requests === true) {
-    // run this once per request:
-    server.events.on('response', (request) => {
-      const data = {
-        event: 'request',
-        method: request.method,
-        path: request.url.path,
-        id: request.id,
-        timestamp: request.info.received,
-        query: Object.assign({}, request.query),
-        userAgent: (request.headers) ? request.headers['user-agent'] : '',
-        browser: (request.headers) ? userAgentLib.parse(request.headers['user-agent']).toString() : '',
-        statusCode: request.response.statusCode,
-        instance: request.info.uri,
-        responseTime: Date.now() - request.info.received,
-        info: request.info,
-        httpVersion: request.raw.req.httpVersion
-      };
-      const tags = ['request'];
-      if (request.response.statusCode >= 500) {
-        tags.push('error');
-      } else if (request.response.statusCode >= 400) {
-        tags.push('warning');
-      } else if (request.response.statusCode >= 300) {
-        tags.push('notice');
-      }
-      log(tags, data, {});
-    });
-  }
 };
 
 exports.plugin = {
--- test/server.test.js
+++ test/server.test.js
@@ -2,94 +2,24 @@
 const Hapi = require('hapi');
 const code = require('code');
 const lab = exports.lab = require('lab').script();
-const Boom = require('boom');
 
 const wait = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
 lab.test('test server is initialized ', async () => {
   const server = new Hapi.Server({ port: 8081 });
+  let called = false;
   await server.register({
     plugin: require('../'),
     options: {
-      async logger(input) {
+      logger(input) {
         code.expect(input).to.include('[start] {"message":"server started","uri":"http://');
-        await server.stop();
+        called = true;
       }
     }
   });
   await server.start();
   server.log(['start'], { message: 'server started', uri: server.info.uri });
   await wait(500);
-});
-
-lab.test('option to log all routes ', async() => {
-  const all = [];
-  const server = new Hapi.Server({ port: 8081 });
-  await server.register({
-    plugin: require('../'),
-    options: {
-      requests: true,
-      logger(output) {
-        // get the logged objects back as json and save them:
-        all.push(JSON.parse(output.split(']')[1]));
-      }
-    }
-  });
-  server.route({
-    method: 'GET',
-    path: '/test',
-    handler(request, h) {
-      return 'hello';
-    }
-  });
-  server.route({
-    method: 'GET',
-    path: '/error',
-    handler(request, h) {
-      throw new Error('hello');
-    }
-  });
-  server.route({
-    method: 'GET',
-    path: '/warning',
-    handler(request, h) {
-      throw Boom.notFound('hello');
-    }
-  });
-  server.route({
-    method: 'GET',
-    path: '/notice',
-    handler(request, h) {
-      return h.redirect('hello');
-    }
-  });
-  await server.start();
-  await server.inject({
-    url: '/test?query=param1',
-    method: 'GET'
-  });
-  await server.inject({
-    url: '/notice',
-    method: 'GET'
-  });
-  await server.inject({
-    url: '/warning',
-    method: 'GET'
-  });
-  await server.inject({
-    url: '/error',
-    method: 'GET'
-  });
-  // wait a bit so event lifecycle is completely processed:
-  await wait(500);
   await server.stop();
-  code.expect(all.length).to.equal(4);
-  code.expect(Object.keys(all[0])).to.equal(['event', 'method', 'path', 'timestamp', 'query', 'userAgent', 'browser', 'statusCode', 'responseTime', 'info', 'httpVersion']);
-  code.expect(all[0].path).to.equal('/test?query=param1');
-  code.expect(Object.keys(all[1])).to.equal(['event', 'method', 'path', 'timestamp', 'query', 'userAgent', 'browser', 'statusCode', 'responseTime', 'info', 'httpVersion']);
-  code.expect(all[1].path).to.equal('/notice');
-  code.expect(Object.keys(all[2])).to.equal(['event', 'method', 'path', 'timestamp', 'query', 'userAgent', 'browser', 'statusCode', 'responseTime', 'info', 'httpVersion']);
-  code.expect(all[2].path).to.equal('/warning');
-  code.expect(Object.keys(all[3])).to.equal(['event', 'method', 'path', 'timestamp', 'query', 'userAgent', 'browser', 'statusCode', 'responseTime', 'info', 'httpVersion']);
-  code.expect(all[3].path).to.equal('/error');
+  code.expect(called).to.equal(true);
 });
